f87839c5e1e4f55df4b25aad2b6de28fc29f7935
Aatir Manzur <aatrapps@gmail.com>
2026-02-08 12:33:36 -0500

    cli pusblish to subscribers from cli

--- cli.cpp
+++ cli.cpp
@@ -58,7 +58,7 @@ void cmd_pub(int argc, char **argv) {
     }
 
     if (key && val) {
-	while(1){
+//	while(1){
         printf("Setting changed: %s = %s\n", key, val);
 	printf("Cap'n publisher %s = %s\n", key, val);
 	capnp::MallocMessageBuilder message;
@@ -68,10 +68,10 @@ void cmd_pub(int argc, char **argv) {
 	auto words = capnp::messageToFlatArray(message);
 	auto bytes = words.asBytes();
 	zmq::message_t zmq_msg(bytes.begin(), bytes.size());
-	publisher.send(zmq_msg, zmq::send_flags::none);
 	sleep(1);
+	publisher.send(zmq_msg, zmq::send_flags::none);
 	printf("send ID: %i \n", id-1);
-    }
+  //  }
     } else {
         printf("Usage: config -k <key> -v <value>\n");
     }
@@ -141,9 +141,36 @@ void run_console() {
     }
 }
 
+// Subscriber Daemon: Receives and deserializes Cap'n Proto messages
+void subscriber_daemon(zmq::context_t* context, std::string addr) {
+    zmq::socket_t sub(*context, zmq::socket_type::sub);
+    sub.connect(addr);
+    sub.set(zmq::sockopt::subscribe, ""); // Subscribe to all topics
+
+    while (true) {
+        zmq::message_t z_msg;
+        auto result = sub.recv(z_msg, zmq::recv_flags::none);
+        
+	size_t wordCount = z_msg.size() / sizeof(capnp::word);
+	auto aligned_buffer = kj::heapArray<capnp::word>(wordCount);
+	memcpy(aligned_buffer.asBytes().begin(), z_msg.data(), z_msg.size());
+
+        capnp::FlatArrayMessageReader reader(aligned_buffer);
+        auto log = reader.getRoot<HelloWorld>();
+
+        std::cout << "[" << addr << "] Recv ID: " << log.getId() 
+                  << " Text: " << log.getMessage().cStr() << std::endl;
+    }
+}
+
 int main(int argc, char *argv[]) {
     // Note: ketopt could still be used here for initial app-wide startup flags
     // e.g., ./myprog -v (verbose mode)
+    zmq::context_t context(1); // One context for the entire manager
+    std::vector<std::thread> daemons;
+    // Start multiple daemons as threads
+    daemons.emplace_back(subscriber_daemon, &context, "tcp://localhost:5555");
+    //for (auto& t : daemons) t.create();
     run_console();
     return 0;
 }
